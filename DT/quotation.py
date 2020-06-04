fe9c4e3c413086364d113144a228a8e6a54ca3f0
zhangfelix <zhangnfelix@gmail.com>
2020-06-04 15:22:48 +0800

    start strategy class program

--- DT/quotation.py
+++ DT/quotation.py
@@ -1,6 +1,31 @@
-import tqsdk
-from config_se import simAccoutName,simAccoutPassWord
-api = tqsdk.TqApi(tqsdk.TqAccount("快期模拟",simAccoutName,simAccoutPassWord))
-print("账户资金：/n")
-print(api.get_account())
-api.close()
+"""
+todo
+"""
+import sys
+from tqsdk import TqApi, TargetPosTask, TqAccount
+from tqsdk.tafunc import ma
+from tqsdk import BacktestFinished
+from datetime import date
+from tqsdk import TqApi, TqSim, TqBacktest
+from config_se import config_se
+from aberration import aberration
+
+def main(argv=None):
+    """
+    测试脚本内容
+    """
+    if argv is None:
+        argv = sys.argv
+    # acc = TqAccount("快期模拟", config_se['simAccoutName'], \
+    #     config_se['simAccoutName'])
+    acc = TqSim()
+    api = TqApi(acc)
+
+    try:
+        # 执行策略方法
+        aberration(api, "xxx")
+        api.close()
+    except BacktestFinished:
+        print(acc.trade_log)
+if  __name__ == "__main__":
+    sys.exit(main())
